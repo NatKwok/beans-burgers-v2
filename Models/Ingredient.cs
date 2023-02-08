04912316b27d39b33fb1d94077cecc210281085a
NatKwok <natkwok902@gmail.com>
2023-02-08 11:09:58 -0500

    GregEdit

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -7,11 +7,10 @@ namespace BeansBurgers_v2.Models {
 
         [Key]
         public int Id {get; set;}
-        public string Type {get; set;}
-        public string Description {get; set;}
+        public string Name {get; set;} // Name instead
         public string PictureURL {get; set;}
         public double ExtraCost {get; set;}
-        public string Category {get; set;}
+        public int Category {get; set;}
 
     }
 }
--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -9,7 +9,7 @@ namespace BeansBurgers_v2.Models {
         public int Id {get; set;}
         public string Name {get; set;}
         public double Price {get; set;}
-        public string Description {get; set;}
+        //public string Description {get; set;}
         public ICollection<Ingredient> Ingredients {get; set;}
 
         // enum Category { Dish Side, Burger, Drink Other}
