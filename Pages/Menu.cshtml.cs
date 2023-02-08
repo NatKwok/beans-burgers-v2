4f5353a6e30af77e698bb018ec14b972f8cda110
MLR-git <MLR-git@users.noreply.github.com>
2023-02-08 15:48:22 -0500

    working on cart

--- Pages/Menu.cshtml.cs
+++ Pages/Menu.cshtml.cs
@@ -7,17 +7,27 @@ namespace BeansBurgers_v2.Pages
 {
     public class MenuModel : PageModel
     {
-        private ApplicationDbContext db;
+        public ApplicationDbContext db;
         public MenuModel(ApplicationDbContext db) => this.db = db;
 
         public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
         public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>(); 
         public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public OrderItem addedItem { get; set; }
         
         public async Task OnGetAsync(){
             Ingredients = await db.Ingredients.ToListAsync();
             MenuItems = await db.MenuItems.ToListAsync();
             OrderItems = await db.OrderItems.ToListAsync();
+        }
+
+        public async Task OnPostAsync(){
+            string itemIndex = Request.Form["Order"];
+            int itemInt = Int32.Parse(itemIndex);
+
+            addedItem.MenuItem = db.MenuItems.ElementAt(itemInt);
+            db.OrderItems.Add(addedItem);
         }
     }
 }
