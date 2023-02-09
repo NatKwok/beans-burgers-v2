f17a973a1785c6d257728663aa7abb57e110afa7
MLR-git <MLR-git@users.noreply.github.com>
2023-02-09 12:57:14 -0500

    Fixes

--- Data/Migrations/20230209175341_Thursday.Designer.cs
+++ Data/Migrations/20230209175341_Thursday.Designer.cs
@@ -4,6 +4,7 @@ using BeansBurgers_v2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeansBurgersv2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230209175341_Thursday")]
+    partial class Thursday
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
