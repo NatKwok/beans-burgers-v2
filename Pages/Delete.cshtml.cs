bde717a88a7b09521d3dc8518b465b9f316b944a
MLR-git <MLR-git@users.noreply.github.com>
2023-02-11 17:48:00 -0500

    workaround for redirect

--- Pages/Delete.cshtml.cs
+++ Pages/Delete.cshtml.cs
@@ -2,6 +2,7 @@ using BeansBurgers_v2.Data;
 using BeansBurgers_v2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeansBurgers_v2.Pages
 {
@@ -9,7 +10,9 @@ namespace BeansBurgers_v2.Pages
     {
         private ApplicationDbContext db;
         public DeleteModel(ApplicationDbContext db) => this.db = db;
-
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>(); 
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>(); 
+        
         [BindProperty(SupportsGet =true)]
         public int Id { get; set; }
 
@@ -17,7 +20,23 @@ namespace BeansBurgers_v2.Pages
             OrderItem item = new OrderItem() { Id = Id };
             db.OrderItems.Remove(item);
             db.SaveChanges();
-            RedirectToPage("Menu");
+            MenuItems = await db.MenuItems.ToListAsync();
+            OrderItems = await db.OrderItems.ToListAsync();
+            Redirect("http://localhost:5280/Menu");
+        }
+
+        public async Task<IActionResult> OnPostAsync(){
+            string itemIndex = Request.Form["Order"];
+            int itemInt = Int32.Parse(itemIndex);
+            MenuItem add = db.MenuItems.ToList().ElementAt(itemInt - 1);
+
+            OrderItem cartItem = new OrderItem { CustomBurger = add.Name, BurgerPrice = (float)add.Price, Quantity = 1 };
+            db.OrderItems.Add(cartItem);
+            db.SaveChanges();
+
+            MenuItems = await db.MenuItems.ToListAsync();
+            OrderItems = await db.OrderItems.ToListAsync();
+            return RedirectToPage("Menu");
         }
     }
 }
