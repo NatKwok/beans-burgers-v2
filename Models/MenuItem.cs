542c31961258d2c24110da783d4fa05daeb4d017
NatKwok <natkwok902@gmail.com>
2023-02-10 23:48:13 -0500

    RadioButtons for Enums

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -21,9 +21,13 @@ namespace BeansBurgers_v2.Models {
     }
      public enum BunsEnum
         {
+            [Display(Name = "Brioche")]
             Brioche = 0,
+            [Display(Name = "Ciabatta")]
             Ciabatta = 1,
+            [Display(Name = "Sesame Seed")]
             Sesame_Seed = 2,
+            [Display(Name = "Poppy Seed")]
             Poppy_Seed = 3,
             
         }
