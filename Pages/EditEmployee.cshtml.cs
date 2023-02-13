53ce0d51dcbe2e4b97f448bed32bab368a696ae2
MLR-git <MLR-git@users.noreply.github.com>
2023-02-13 18:44:09 -0500

    Edit Employee scaffold

--- Pages/EditEmployee.cshtml.cs
+++ Pages/EditEmployee.cshtml.cs
@@ -1,29 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using BeansBurgers_v2.Data;
 using BeansBurgers_v2.Models;
+using BeansBurgers_v2.Data;
 using Microsoft.AspNetCore.Identity;
 
 namespace BeansBurgers_v2.Pages
 {
-    public class DeleteEmployeeModel : PageModel
+    public class EditEmployeeModel : PageModel
     {
         private ApplicationDbContext db;
-        public DeleteEmployeeModel(ApplicationDbContext db) => this.db = db;
+        public EditEmployeeModel(ApplicationDbContext db) => this.db = db;
         public List<IdentityUser> Users { get; set; } = new List<IdentityUser>();
         
         [BindProperty(SupportsGet =true)]
         public string Id { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(){
+        public void OnGet(){
             Users = db.Users.ToList();
-            foreach(IdentityUser user in Users){
-                if(user.Id == Id){
-                    db.Users.Remove(user);
-                }
-            }
-            db.SaveChanges();
+        }
+
+        public async Task<IActionResult> OnPostAsync(){
+
+             
+            
             return RedirectToPage("Employees");
         }
     }
 }
+
