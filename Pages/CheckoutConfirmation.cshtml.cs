b60476e31dd6d25f59aac7c3bbf4a68c041148cf
MLR-git <MLR-git@users.noreply.github.com>
2023-02-14 09:57:44 -0500

    checkout fixes

--- Pages/CheckoutConfirmation.cshtml.cs
+++ Pages/CheckoutConfirmation.cshtml.cs
@@ -11,8 +11,13 @@ namespace BeansBurgers_v2.Pages
         private ApplicationDbContext db;
         public CheckoutConfirmationModel(ApplicationDbContext db) => this.db = db;
         public OrderDetails OrderDetails {get; set;} = new OrderDetails();
+        public List<OrderItem> OrderItems {get; set;} = new List<OrderItem>();
         public async Task<IActionResult> OnGetAsync(int? id)
         {
+            OrderItems = await db.OrderItems.ToListAsync();
+            foreach(OrderItem item in OrderItems){
+                Console.WriteLine(item.CustomBurger);
+            }
             OrderDetails = await db.OrdersDetails.Where(od => od.Id == id).Include(OrderDetails => OrderDetails.OrderItemsList).FirstOrDefaultAsync();
             return Page();
         }
