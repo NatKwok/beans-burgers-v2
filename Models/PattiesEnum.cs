5fc273e22b455015062ce4d639553048a3868627
MLR-git <MLR-git@users.noreply.github.com>
2023-02-13 23:53:52 -0500

    customize edits

---
+++ Models/PattiesEnum.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BeansBurgers_v2.Pages
+{
+public enum PattiesEnum
+    {
+        Angus = 0,
+        Fried_Chicken = 1,
+        Fish_Cake = 2,
+        Bison = 3,
+        Beyond_Meat = 4,
+        Portobello = 5
+    }
+}
